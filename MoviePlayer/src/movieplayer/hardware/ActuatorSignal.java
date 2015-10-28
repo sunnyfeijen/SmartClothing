4a1aa7c3b18fb293a502eca079c06d2e7ec76e62
Meny22 <meny.metekia@gmail.com>
2015-10-28 16:01:22 +0100

    Actuator preperations

---
+++ MoviePlayer/src/movieplayer/hardware/ActuatorSignal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieplayer.hardware;
+
+import movieplayer.hardware.Actuator;
+
+/**
+ *
+ * @author Sasa2905
+ */
+public class ActuatorSignal {
+    private int id;
+    //Start-time of signal in ms
+    private int startTime;
+    //position of actuator on body;
+    private int position;
+    //type of actuator
+    private Actuator actuatorType;
+    
+}
